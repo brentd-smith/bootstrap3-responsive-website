080d2a7cd8cda0bdcdddcfce97923c6521fb55c7
Brent Smith <bdchan305@gmail.com>
2016-06-24 17:08:22 +0000

    finished up through lecture on tabbable content

--- boot3/static/includes/js/script.js
+++ boot3/static/includes/js/script.js
@@ -8,3 +8,20 @@ Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
+$(function() {
+
+    $('#alertMe').click(function(e) { 
+        
+        /* default behavior is to nav
+           to the href or refresh the page */
+        //e.preventDefault();
+        $('#successAlert').toggle("slow");
+        
+        // $('#successAlert').slideDown();
+    });
+    
+    $('#closeAlert').click(function(e) {
+        e.preventDefault();
+        $('#successAlert').toggle("slow");
+    });
+});
